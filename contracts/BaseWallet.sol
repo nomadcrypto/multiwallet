8877089dcd3f7cd40d8a96b69013a4a15173afab
= <nomadcrypto@gmail.com>
2019-03-25 10:21:59 -1000

    added LICENSE and documentation

--- contracts/BaseWallet.sol
+++ contracts/BaseWallet.sol
@@ -4,18 +4,9 @@ import 'openzeppelin-solidity/contracts/token/ERC721/IERC721.sol';
 
 
 /**
-* @title Wallet
-* @dev a simple multi-user wallet
-*
-*/
-
-/*
-overview:
-
-	The wallet will have 3 main types of assets(eth, erc20 token, or erc721 token)
-
-Variables needed for the wallet.
-mapping from address to array of 
+* @title BaseWallet
+* @dev a simple multi-user and multi-token wallet with support for ETH, ERC20 and ERC721 Tokens
+* @author nomadcrypto@gmail.com
 */
 contract BaseWallet {
 
@@ -74,13 +65,8 @@ contract BaseWallet {
 	mapping(string => uint256) internal tokenSymbolToID;
 	//mapping from token symbol to bool for easy checking if registered
 	mapping(string => bool) internal tokenSymbolRegistered; 
-
-
 	//mapping from token id to total balance
 	mapping(uint256=>uint256) internal totalBalance;
-
-
-
 	//mapping from address to token id to balance
 	mapping(address => mapping(uint256 => uint256)) internal balance;
 	//mapping from address to token id to array of non_fungible token ids
@@ -94,37 +80,33 @@ contract BaseWallet {
 
 
 
-	/*
-	* @dev
+	/**
+	* @dev throws if called by any account other than the owner
 	*/
-
 	modifier onlyOwner() {
 		require(msg.sender == owner);
 		_;
 	}
 
-	/*
-	* @dev
+	/**
+	* @dev throws if token isn't a valid token type
 	*/
-
 	modifier onlyValidTokenType(uint token_type) {
 		require(token_type == uint(TokenType.ETH) || token_type == uint(TokenType.ERC20) || token_type == uint(TokenType.ERC721), "Invalid token type");
 		_;
 	}
 
-	/*
-	* @dev
+	/**
+	* @dev thows if symbol is not already registered
 	*/
-
 	modifier onlyRegsteredToken(string memory symbol) {
 		require(tokenSymbolRegistered[symbol] == true, "Token is not registered");
 		_;
 	}
 
-	/*
-	* @dev
+	/**
+	* @dev  throws if the token has already been registered
 	*/
-
 	modifier onlyUnRegisteredToken(string memory symbol) {
 		require(tokenSymbolRegistered[symbol] == false, "Token already registered");
 		_;
@@ -134,7 +116,7 @@ contract BaseWallet {
 		owner = msg.sender;
 	}
 
-	/*
+	/**
 	* @dev add the token
 	* @param token_address the address of the token or 0x in the case of eth
 	* @param name the name of the token, e.g. Etherum, SimpleToken, CryptoKitties, etc
@@ -168,38 +150,41 @@ contract BaseWallet {
 
 	}
 
-	/*
+	/**
 	* @dev gets the token id by the given symbol
 	* @param symbol the symbol, e.g. eth, eos, etc
 	* @return A uint256 token id for the given symbol
 	*/
-
 	function getTokenIDBySymbol(string memory symbol) onlyRegsteredToken(symbol) public view returns (uint256) {
 		return tokenSymbolToID[symbol];
 	}
 
-	/*
+	/**
 	* @dev gets the token id by index
 	* @param index the index of the symbol, e.g. 0,1, etc. useful for UIs
 	* @return A uint256 token id for the given index
 	*/
-
 	function getTokenIDByIndex(uint256 index) public view returns (uint256) {
 		require(index < tokens.length);
 		return tokenIndexToID[index];
 	}
 
-	/*
+	/**
 	* @dev gets the token index by id
 	* @param id the token id for the given token
 	* @return A uint256 index for the given token id
 	*/
-
 	function getTokenIndexByID(uint256 id) public view returns (uint256) {
 		return tokenIDToIndex[id];
 	}
 
-
+	/**
+	* @dev updates the balance of the msg.sender
+	* @param tokenid the tokenid
+	* @param index the index
+	* @param amount the amount
+	* @param credited if the amount is a credit or debit
+	*/
 	function _updateBalance(uint256 tokenid, uint256 index, uint256 amount, bool credited) internal {
 		uint256 totalBalancePre = totalBalance[tokenid];
 		uint256 accountBalance = balance[msg.sender][tokenid];
@@ -216,6 +201,13 @@ contract BaseWallet {
 
 	}
 
+	/**
+	* @dev adds a non-fungible token
+	* @param account the address of the account we're adding the token to
+	* @param tokenid the internal tokenid of the token being added
+	* @param  index the index of the token being added
+	* @param external_id the external tokenid
+	*/
 	function _addNonFungible(address account, uint256 tokenid, uint256 index, uint256 external_id) internal {
 		uint256 tindex = nonfungibleTokens[account][tokenid].length;
 		nonfungibleTokens[account][tokenid].push(external_id);
@@ -224,12 +216,25 @@ contract BaseWallet {
 		nonfungibleTokenRegistered[account][tokenid][external_id] = true;
 	}
 
+	/**
+	* @dev deposits a non-fungbile token
+	* @param tokenid the token id of the token being deposited
+	* @oaram index the index of the token being deposited
+	* @param external_id the external id of the token being deposited
+	*/
 	function _depositNonFungible(uint256 tokenid, uint256 index, uint256 external_id) internal {
 		_updateBalance(tokenid, index, 1, true);
 		_addNonFungible(msg.sender, tokenid, index, external_id);
 
 	}
 
+	/**
+	* @dev removes a non-fungible token
+	* @param account the address of the account we are removing the token from
+	* @param tokenid the internal token id of the token being removed
+	* @oaram index the internal index of the token being removed
+	* @param external_id the external id of the token being removed
+	*/
 	function _removeNonFungible(address account, uint256 tokenid, uint256 index, uint256 external_id) internal {
 		nonfungibleTokenRegistered[account][tokenid][external_id] = false;
 		if(nonfungibleTokens[account][tokenid].length == 1) {
@@ -253,12 +258,25 @@ contract BaseWallet {
 
 	}
 
-
+	/**
+	* @dev deposits ETH
+	* @param symbol the symbol being added(always ETH - just for compatiblity with other tokens)
+	* @param tokenid the internal token id for ETH
+	* @param index the internal index of the token
+	* @param amount the amount the token being deposited
+	*/
 	function _depositEth(string memory symbol, uint256 tokenid, uint256 index, uint256 amount) internal {
 		_updateBalance(tokenid, index, amount, true);
 		emit DepositedETH(msg.sender, amount);
 	}
 
+	/**
+	* @dev deposits an ERC20 token
+	* @param symbol the symbol of the token being deposited
+	* @param tokenid the internal tokenid of the token being deposited
+	* @param index the internal index of the token being deposited
+	* @param amount the amount of the token being deposited
+	*/
 	function _depositERC20(string memory symbol, uint256 tokenid, uint256 index, uint256 amount) internal {
 		//here we access the given erc20 token and transfer the amount previously alotted to us. only if that succeeds 
 		//then we deposit the fungible token and update balances accordingly
@@ -270,6 +288,13 @@ contract BaseWallet {
 		}
 	}
 
+	/**
+	* @dev deposits an ERC721 token 
+	* @param symbol the symbol of the token being deposited
+	* @param tokenid the internal tokenid for the token being deposited
+	* @param index the internal index for the token being deposited
+	* @param external_id the external id of the token being deposited
+	*/
 	function _depositERC721(string memory symbol, uint256 tokenid, uint256 index, uint256 external_id) internal {	
 		//here we access the given erc271 token and transfer the given external id to us. only if that succeeds 
 		//then we update the overall balance for that token as well as add it to the nonfungible tokens in the accounts
@@ -281,6 +306,11 @@ contract BaseWallet {
 
 	}
 
+	/**
+	* @dev deposit a token
+	* @param symbol the symbol of the token being deposited
+	* @param amount the amount, or tokenid in the case of a non-fungible token, that is being deposited
+	*/
 	function deposit(string memory symbol, uint256 amount) onlyRegsteredToken(symbol) public payable {
 		uint256 tokenid = tokenSymbolToID[symbol];
 		uint256 index = tokenIDToIndex[tokenid];
@@ -295,11 +325,24 @@ contract BaseWallet {
 
 	}
 
+	/**
+	* @dev gets the balance of the given address for the given symbol
+	* @param _address the address of the account being checked
+	* @param symbol the symbol of the token being checked
+	* @return the balance for the given address and token symbol
+	*/
 	function balanceOf(address _address, string memory symbol) onlyRegsteredToken(symbol) public view returns (uint256) {
 		uint256 tokenid = tokenSymbolToID[symbol];
 		return balance[_address][tokenid];
 	}
 
+	/**
+	* @dev withdraws ETH
+	* @param symbol the symbol of the token (e.g. ETH)
+	* @param tokenid the internal tokenid
+	* @param index the internal index of the token
+	* @param amount the amount of the given token being withdrawn
+	*/
 	function _withdrawEth(string memory symbol, uint256 tokenid, uint256 index, uint256 amount) internal {
 		require(balance[msg.sender][tokenid] >= amount);
 		_updateBalance(tokenid, index, amount, false);
@@ -308,6 +351,13 @@ contract BaseWallet {
 
 	}
 
+	/**
+	* @dev withdraws an ERC20 token
+	* @param symbol the symbol of the token being withdrawn
+	* @param tokenid the internal tokenid 
+	* @param index the internal token index
+	* @param amount the amount of the token being withdrawn
+	*/
 	function _withdrawERC20(string memory symbol, uint256 tokenid, uint256 index, uint256 amount) internal {
 		require(balance[msg.sender][tokenid] >= amount);
 		IERC20 c = IERC20(tokens[index].token_address);
@@ -316,6 +366,13 @@ contract BaseWallet {
 		emit WithdrewERC20(msg.sender, symbol, amount);
 	}
 
+	/**
+	* @dev withdraws an ERC721 token
+	* @param symbol the symbol of the token being withdrawn
+	* @param tokenid the internal tokenid
+	* @param index the internal token index
+	* @param external_id the external id of the token being withdrawn
+	*/
 	function _withdrawERC721(string memory symbol, uint256 tokenid, uint256 index, uint256 external_id) internal {
 		require(nonfungibleTokenRegistered[msg.sender][tokenid][external_id] == true);
 		IERC721 c = IERC721(tokens[index].token_address);
@@ -325,6 +382,11 @@ contract BaseWallet {
 		emit WithdrewERC721(msg.sender, symbol, external_id);
 	}
 
+	/**
+	* @dev withdraws the token token
+	* @param symbol the symbol of the token being withdrawn
+	* @param amount the amount being withdrawn
+	*/
 	function withdraw(string memory symbol, uint256 amount) onlyRegsteredToken(symbol) public {
 		uint256 tokenid = tokenSymbolToID[symbol];
 		uint256 index = tokenIDToIndex[tokenid];
@@ -338,12 +400,26 @@ contract BaseWallet {
 		}
 	}
 
+	/**
+	* @dev transfers a fungible token
+	* @param from the address sending the token
+	* @param to the address recieving the token
+	* @param tokenid the internal tokenid
+	* @param amount the amount being transfered
+	*/
 	function _transferFungible(address from, address to, uint256 tokenid, uint256 amount) internal {
 		require(balance[from][tokenid] >= amount);
 		balance[from][tokenid] -= amount;
 		balance[to][tokenid] += amount;
 	}
 
+	/**
+	* @dev transfers a non-fungbile token
+	* @param from the address sending the token
+	* @param to the address recieving the token
+	* @param tokenid the internal tokenid
+	* @param external_id the external tokenid
+	*/
 	function _transferNonFungible(address from, address to, uint256 tokenid, uint256 external_id) internal {
 		require(nonfungibleTokenRegistered[msg.sender][tokenid][external_id] == true);
 		uint256 index = tokenIDToIndex[tokenid];
@@ -353,6 +429,12 @@ contract BaseWallet {
 		balance[to][tokenid] += 1;
 	}
 
+	/**
+	* @dev transfers the given token to the given address
+	* @param account the address the token is being transfered to
+	* @param symbol the symbol of the token being transfered
+	* @param amount the amount, or tokenid in the case of non-fungible tokens, being transfered
+	*/
 	function transfer(address account, string memory symbol, uint256 amount) public {
 		uint256 tokenid = tokenSymbolToID[symbol];
 		if(tokens[tokenIDToIndex[tokenid]].fungible){
@@ -362,6 +444,4 @@ contract BaseWallet {
 		}
 		emit Transfer(msg.sender, account, symbol, amount);
 	}
-
-
 }
